7e967fbb63c186261ded8f4924e411aba577dc05
Chaitanya Joshi <chaitanyajoshi@Chaitanyas-Air.cable.rcn.com>
2023-08-28 23:32:24 -0400

    added docker support

---
+++ BcgCodingChallenge/BcgCodingChallengeTestApp/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿namespace BcgCodingChallengeTestApp;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
