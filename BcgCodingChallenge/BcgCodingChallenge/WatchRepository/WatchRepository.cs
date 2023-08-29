ee085a98e433112575c782e4ed3577df1df1c9e9
Chaitanya Joshi <chaitanyajoshi@Chaitanyas-Air.cable.rcn.com>
2023-08-28 20:37:42 -0400

    error handling and separating lookup logic

--- BcgCodingChallenge/BcgCodingChallenge/WatchRepository/WatchRepository.cs
+++ BcgCodingChallenge/BcgCodingChallenge/WatchRepository/WatchRepository.cs
@@ -3,78 +3,82 @@ using BcgCodingChallenge.WatchHelper;
 
 namespace BcgCodingChallenge.WatchRepository
 {
-	public class WatchRepository:IWatchRepository
-	{
-		//private readonly Dictionary<string, Watch> watchLookup = new Dictionary<string, Watch>();
+    public class WatchRepository : IWatchRepository
+    {
+        private static readonly Dictionary<string, Watch> watchLookup = new Dictionary<string, Watch>();
 
-  //      public WatchRepository()
-  //      {
-
-  //      }
-
-  //      public void PopulateWatchLookup()
-  //      {
-
-  //      }
-        public bool GetTotalCostFromCart(List<string> watches,out string result)
+        static WatchRepository()
         {
-            
-            double totalCost = 0;
-            Dictionary<string, int> watchCart = new Dictionary<string, int>();
-            foreach (string watch in watches)
-            {
-                if (watchCart.ContainsKey(watch))
-                {
-                    watchCart[watch] = watchCart[watch] + 1;
-                }
-                else
-                {
-                    watchCart.Add(watch, 1);
-                }
-            }
+            PopulateWatchLookup();
+        }
 
-            Dictionary<string, Watch> watchLookup = new Dictionary<string, Watch>();
-            //#TODO - separate this logic
+        public static void PopulateWatchLookup()
+        {
+            watchLookup.Clear();
             watchLookup.Add("001", new Watch() { WatchId = 001, WatchName = "Rolex", UnitPrice = 100, Discount = new Discount() { DiscountQuantity = 3, DiscountPrice = 200 } });
             watchLookup.Add("002", new Watch() { WatchId = 002, WatchName = "Michael Kors", UnitPrice = 80, Discount = new Discount() { DiscountQuantity = 2, DiscountPrice = 120 } });
             watchLookup.Add("003", new Watch() { WatchId = 003, WatchName = "Swatch", UnitPrice = 50 });
             watchLookup.Add("004", new Watch() { WatchId = 004, WatchName = "Casio", UnitPrice = 30 });
+        }
 
-            foreach (KeyValuePair<string, int> item in watchCart)
+        public bool GetTotalCostFromCart(List<string> watches, out string result)
+        {
+            double totalCost = 0;
+            Dictionary<string, int> watchCart = new Dictionary<string, int>();
+            try
             {
-                if (watchLookup != null)
+
+                foreach (string watch in watches)
                 {
-                    if (watchLookup.ContainsKey(item.Key))
+                    if (watchCart.ContainsKey(watch))
                     {
-                        Watch watch = watchLookup[item.Key];
-                        //if (watchLookup[item.Key].Discount != null)
-                        if (watch.Discount != null)
-                        {
-                            //Watch w = watchLookup[item.Key];
-                            //int q = item.Value / watchLookup[item.Key].Discount.DiscountQuantity;
-                            int q = item.Value / watch.Discount.DiscountQuantity;
-                            int r = item.Value % watch.Discount.DiscountQuantity;
-                            totalCost = totalCost + (q * watch.Discount.DiscountPrice) + (r * watch.UnitPrice);
-                        }
-                        else
-                        {
-                            //totalCost = totalCost + item.Value * watchLookup[item.Key].UnitPrice;
-                            totalCost = totalCost + item.Value * watch.UnitPrice;
-                        }
+                        watchCart[watch] = watchCart[watch] + 1;
+                    }
+                    else
+                    {
+                        watchCart.Add(watch, 1);
                     }
                 }
 
+                foreach (KeyValuePair<string, int> item in watchCart)
+                {
+                    if (watchLookup != null)
+                    {
+                        if (watchLookup.ContainsKey(item.Key))
+                        {
+                            Watch watch = watchLookup[item.Key];
+                            
+                            if (watch.Discount != null)
+                            {
+                                int discountedWatches = item.Value / watch.Discount.DiscountQuantity;
+                                int regularWatches = item.Value % watch.Discount.DiscountQuantity;
+                                totalCost = totalCost + (discountedWatches * watch.Discount.DiscountPrice) + (regularWatches * watch.UnitPrice);
+                            }
+                            else
+                            {
+                                totalCost = totalCost + item.Value * watch.UnitPrice;
+                            }
+                        }
+                    }
+
+                }
+                if (totalCost == 0)
+                {
+                    result = "All watches added to the cart are invalid";
+                    return true;
+                }
+                //return Json(new { price = totalCost });
+                result = totalCost.ToString();
+                return false;
+
             }
-            if (totalCost == 0)
+            catch (Exception ex)
             {
-                result = "All watches added to the cart are invalid";
+                result = ex.Message;
                 return true;
             }
-            //return Json(new { price = totalCost });
-            result = totalCost.ToString();
-            return false;
+
         }
-    
     }
 }
 
